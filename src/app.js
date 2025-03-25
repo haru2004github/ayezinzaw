2bbbbd23b3b8b502ff3a4449985bd97571aeeee0
Bhone Pyae Sone <hponepyaesone9620@gmail.com>
2025-03-25 11:18:58 +0630

    first commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+
+
+// scroll reval
+ScrollReveal().reveal('.headline',{
+    origin: 'bottom',
+    distance: '60px' ,
+    duration : 450,
+    interval: 200,
+    delay:200,
+    reset : false,
+   
+   });
+
